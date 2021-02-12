9f6175fb9dab69237540fc54e1ab7f604e816dce
perkdoc <78950771+perkdoc@users.noreply.github.com>
2021-02-12 02:21:08 -0500

    Update Instagram Report Bot2.py

--- Instagram Report Bot2.py
+++ Instagram Report Bot2.py
@@ -23,7 +23,7 @@ username = args.username
 acc_file = args.file
 
 if username == "" :
-	username = input("Username: ")
+	username = input("Username:perkdoctor ")
 
 a = open(acc_file, "r").readlines()
 file = [s.rstrip()for s in a]
